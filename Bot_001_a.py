35da0b3ac632ff3ebd8f858414dc1c0a41300180
Taylor Edwin Nichols <tayloredwinnichols@Taylors-MacBook-Pro.local>
2022-01-03 20:01:55 -0500

    committing after making bot_002

--- Bot_001_a.py
+++ Bot_001_a.py
@@ -1,57 +1,138 @@
 import alpaca_trade_api as tradeapi
 from datetime import date
 import time
-import datetime
+import requests
+import json
+import pandas as pd
+from datetime import datetime
+from pytz import timezone
 
 
 
+## setting api keys and REST ##
 BASE_URL = 'https://paper-api.alpaca.markets'
-PUB_KEY = 'PK23SND8QLZ211MQPQXI'
-SEC_KEY = '97t5BjP9Ugmym4404zPqRL2js2RWs9PZXt3dhbOr'
+PUB_KEY = 'PK2TSXKJ9TYK7AZOWU18'
+SEC_KEY = 'qvQ7zoUULTXkcObXAB6ipBcPSMi5SSUSRBtsFQQw'
 api = tradeapi.REST(key_id= PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)
 
 
-ticker = "FAS"
+api_key = "af5dfc6f8b230bdf12bfae6008279325"
 
-trading = True; days = 1
-# making a loop that will last for 3 days
-while trading:
-    print("Day {}.".format(days))
+
+
+
+## getting latest stock prices for tickers ## 
+def fetch_most_recent_quote(tickers, api_key):
+    responses = []; fail_resp = []
+    with requests.session() as session:
+        for ticker in tickers:
+            resp = session.get(f"https://financialmodelingprep.com/api/v3/quote-short/"+ticker+"?apikey="+api_key)
+            try:
+                responses.append([resp,resp.json()[0]])
+            except:
+                responses.append([None,None])
+                fail_resp.append(ticker)
+                
+    return responses, fail_resp
+
+
+## getting stock proces from the day before ## 
+def fetch_day_before_quote(tickers, api_key):
+    responses = []; fail_resp = []
+    with requests.session() as session:
+        for ticker in tickers:
+            resp = session.get(f"https://financialmodelingprep.com/api/v3/historical-price-full/"+ticker+"?timeseries=1&apikey="+api_key)
+            try:
+                responses.append([resp,resp.json()])
+            except:
+                responses.append([None,None])
+                fail_resp.append(ticker)
+                
+    return responses, fail_resp
+
+
+
+
+## tickers to execute ##
+tickers = ["FAS","TNA","SOXL","TECL","LABU","YINN","DPST","NAIL"]
+
+## getting day trading day before close price ##
+mrq,fr1 = fetch_most_recent_quote(tickers, api_key)
+
+
+## the script will loop here untill the market opens ##
+clock = api.get_clock() # gets if the market is open
+while not clock.is_open:
+    ct = clock.timestamp
     
-    # waiting for the day to change
-    todayDate = date.today() # setting todays date
-    times = 0 # for testing reasons it will loop 3 times.
-    while date.today() == todayDate: # if it is still the same day it will stay up here in the code
-        print('still the same day')
-        times += 1
-        if times == 3:
-            todayDate = 'not today'
-        time.sleep(.25) # sleeping for 0.5 second so it isn't freaking out
-        
-    print('the day changed')
+    
+print()
+print("The market opened up at timestamp: {}.".format(clock.timestamp))
+    
+    
+## getting the first quote of the day ##
+dbq,fr2 = fetch_day_before_quote(tickers, api_key)
 
-    # Get daily price data for AAPL over the last 5 trading days.
-    barset = api.get_barset(ticker, 'day', limit=5)
-    open_price = barset[ticker][-1].c
-    print('previous day close {}.'.format(open_price))
+## formatting the open and close data
+close_data = []; open_data = []
+for o, c in zip(dbq, mrq):
+    close_data.append(c[1]['price'])
+    open_data.append(o[1]['historical'][0]['close'])
+all_data = {'ticker': tickers, "close_data": close_data, "open_data": open_data}
+all_data = pd.DataFrame(all_data)
+    
+print(all_data)
 
 
-    clock = api.get_clock()
-    times = 0
-    while not clock.is_open:
-        times += 1
-        print('still isn''t open')
-        if times == 3:
-            break
-        
-    print('the market opened up')
+## figuring out what tickers open higher than the closing day before ##
+bid = []
+for i in range(len(all_data)):
+    if all_data.iloc[i]['close_data'] < all_data.iloc[i]['open_data']:
+        bid.append(all_data.iloc[i]['ticker'])
+print("I'll be placing bids on these tickers: {}.".format(bid))
+
+   
+## placing orders for tickers that passed criteria ## 
+for ticker in bid:
+    api.submit_order(
+            symbol=ticker,
+            qty=10,
+            side='buy',
+            type='market',
+            time_in_force='gtc'
+        )
+
+
+
+## wait until these orders have been filled then loop through then resubmit
+
+
+
+# ## waiting till the end of the day before selling aquired shares
+# est = timezone('EST')
+# current_time = datetime.now(est).strftime("%I:%M")
+
+# while not current_time == "04:20":
+#     current_time = datetime.now(est).strftime("%I:%M")
+    
+## submitting at the close order ##
+for ticker in bid:
+    api.submit_order(
+        symbol=ticker,
+        qty=10,
+        side='sell',
+        type='market',
+        time_in_force='cls'
+    )
     
 
-        
-    days += 1
-    if days == 4:
-        trading = False
-    
-    print()
+
+print("Program is complete!!")
+print("Who knows what happened!!!")
+
+
+
+
+
 
 
---
+++ Bot_002.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 14:53:27 2022
+
+@author: tayloredwinnichols
+""" 
+
+import alpaca_trade_api as tradeapi
+from datetime import date
+import time
+import datetime
+import requests
+import json
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+from pytz import timezone
+import pandas_ta as ta
+
+
+BASE_URL = 'https://paper-api.alpaca.markets'
+PUB_KEY = 'PK92I7HHN7R3TNHJSJG8'
+SEC_KEY = 'To6M3zJRMYfaBriBAtPru9E2uSg7MnwPLiy5CO4G'
+api = tradeapi.REST(key_id= PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)
+
+
+
+
+
+## Making a function that takes in ticker and outputs df with ti ##
+def getDF(ticker, LIMIT):
+    # Load data
+    df = api.get_barset(ticker, 'day', limit=LIMIT).df
+    df = df.set_axis(['open', 'high', 'low', 'close', 'volume'], axis=1, inplace=False)
+
+    # add data points
+    df['sma10'] = df.ta.sma(length=10)
+    df['williams'] = df.ta.willr()
+
+    df = df[[
+            'open','close','sma10','williams'
+            ]]
+
+    df = df.dropna()
+    df.reset_index(inplace=True)
+    
+    return df
+
+
+
+## check market time ##
+def time_to_market_close():
+    clock = api.get_clock()
+    return (clock.next_close - clock.timestamp).total_seconds()/60
+
+
+
+
+## check to see if any of tickers have buy signals ##
+def should_buy(tickers):
+    des = []
+    for ticker in tickers:
+        data = getDF(ticker, 60)
+        if data.iloc[-1]['williams'] < -80:
+            des.append([ticker,True])
+        else:
+            des.append([ticker,False])
+    return des
+
+
+
+## check to see if any of tickers have sell signals ##
+def should_sell(tickers):
+    des = []
+    for ticker in tickers:
+        data = getDF(ticker, 60)
+        if data.iloc[-1]['williams'] > -20:
+            des.append([ticker,True])
+        else:
+            des.append([ticker,False])
+    return des
+
+
+
+## check to see if there is a position in ticker ##
+def does_pos(ticker):
+    portfolio = api.list_positions()
+    pos_owned = []
+    for position in portfolio:
+        pos_owned.append(position.symbol)
+    state = ticker in pos_owned
+    return state
+
+
+
+
+## tickers to use ##
+tickers = ["SOXL","FAS","FNGU","TNA","LABU","YINN","DPST","NAIL",\
+           "DFEN","DUSL","MIDU","MEXX","PILL","RETL","TPOR","KORU"]
+
+
+
+## the start of the program will update us on a few things ##
+account = api.get_account()
+print("Account number: {}".format(account.account_number))
+print("Buying power: ${}".format(account.buying_power))
+print("Cash available: ${}".format(account.cash))
+print('${} allowed for each ticker, or {}%.'.format(float(account.cash)/len(tickers),100/len(tickers)))
+print("status: {}".format(account.status))
+print("trading blocked: {}".format(account.trading_blocked))
+print()
+## check current positions ##
+# Get a list of all of our positions.
+portfolio = api.list_positions()
+# Print the quantity of shares for each position.
+for position in portfolio:
+    print("I own {} shares of {}.".format(position.qty, position.symbol))
+
+print()
+
+
+## main loop that runs. it waits will there is 15 minutes left in the trading day ##
+minutes = time_to_market_close()
+while minutes > 15:
+    minutes = time_to_market_close()
+    time.sleep(180)
+    print("current time: {}".format(minutes))
+    
+print()    
+print('it left the loop!!')
+print()
+
+
+
+
+## should I buy any tickers?? ##
+des1 = should_buy(tickers)
+for d in des1:
+    print("Buying descision: {} for ticker: {}.".format(d[1], d[0]))
+print()
+print()
+
+
+## should I sell any tickers?? ##
+des2 = should_sell(tickers)
+for d in des2:
+    print("Selling descision: {} for ticker: {}.".format(d[1], d[0]))
+print()
+print()
+
+
+## if we do not own the ticker, buy order ##
+account = api.get_account()
+for i in des1:
+    if i[1] == True and does_pos(i[0]) == False:
+        
+        # figuring out how many shares to buy #
+        last_price = api.get_last_trade(i[0]).price
+        order_amount = int(((float(account.cash)/len(tickers))*.95)/last_price)
+        
+        # Buy a stock
+        api.submit_order(
+          symbol=i[0], # Replace with the ticker of the stock you want to buy
+          qty=order_amount,
+          side='buy',
+          type='market', 
+          time_in_force='day' # day
+        )
+        
+        print("place buy order for {} and {} shares.".format(i[0], order_amount))
+
+print()
+print()
+
+## if we do own the ticker, sell order ##
+for i in des2:
+    if i[1] == True and does_pos(i[0]) == True:
+        
+        # how many shares to sell #
+        order_amount = int(api.get_position(i[0]).qty)
+        
+        api.submit_order(
+          symbol=i[0], # Replace with the ticker of the stock you want to buy
+          qty=order_amount,
+          side='sell',
+          type='market', 
+          time_in_force='gtc' # Good 'til cancelled
+        )
+        
+        print("place sell order for {} and {} shares.".format(i[0], order_amount))
+
+
+print()
+print()
+
+## print information after script has ran ##
+## list orders and details ##
+orders = api.list_orders()
+print("{} orders in.".format(len(orders)))
+for o in orders:
+    print("Sell {} shares of {} on {}.".format(o.qty, o.symbol, o.time_in_force))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
